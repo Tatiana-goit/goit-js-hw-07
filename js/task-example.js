d933a1661fe2f35f70e23cfb8ee8db211790c4b0
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-04-01 14:02:09 +0300

    примеры

--- js/task-example.js
+++ js/task-example.js
@@ -14,10 +14,30 @@ const titleEl = document.querySelector('h1');
 // console.log(titleEl.textContent);
 
 const navEl = document.querySelector('.site-nav');
-console.log(navEl);
-console.log(navEl.children);
+// console.log(navEl);
+// console.log(navEl.children);
 
 const navItemEl = document.querySelector('.site-nav__item')
-console.log(navItemEl.children);
+// console.log(navItemEl.children);
+
+const navLinkEl = document.querySelector('.site-nav__link');
+// console.log(navLinkEl);
+// console.log(navLinkEl.textContent);
+
+navEl.classList.add('new', 'second');
+navEl.classList.remove('second');
+navEl.classList.replace('new', 'new2');
+// console.log(navEl.classList.contains('new2'));
+navEl.classList.toggle('new')
+// console.log(navEl);
+
+const linkEl = document.querySelector('a');
+console.log(linkEl);
+console.log(linkEl.getAttribute('href'));;
+
+console.log(linkEl.attributes);
+
+
+
 
 
