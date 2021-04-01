72a22e9c131e91d7880d95eaa955a41e7c375380
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-04-01 10:23:53 +0300

    Тренировка

---
+++ js/task-example.js
@@ -0,0 +1,23 @@
+const listEl = document.querySelector('.animal__item');
+// console.log(listEl);
+
+const itemEl = document.querySelectorAll('.animal__item');
+// console.log(itemEl);
+
+// const imgEl = document.querySelector('.people__img');
+// console.log(imgEl);
+// console.log(imgEl.src);
+// imgEl.alt = 'Могу выполнять разные действия';
+
+const titleEl = document.querySelector('h1');
+// console.log(titleEl);
+// console.log(titleEl.textContent);
+
+const navEl = document.querySelector('.site-nav');
+console.log(navEl);
+console.log(navEl.children);
+
+const navItemEl = document.querySelector('.site-nav__item')
+console.log(navItemEl.children);
+
+
