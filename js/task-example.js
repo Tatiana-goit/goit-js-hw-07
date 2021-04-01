edef54e21dc78e211ed9d6403db79f5d058b3ebe
Tatiana Taranushchenko <tanchou74@gmail.com>
2021-04-02 01:26:40 +0300

    Тренировка по видео Репеты

--- js/task-example.js
+++ js/task-example.js
@@ -32,10 +32,62 @@ navEl.classList.toggle('new')
 // console.log(navEl);
 
 const linkEl = document.querySelector('a');
-console.log(linkEl);
-console.log(linkEl.getAttribute('href'));;
+// console.log(linkEl);
+// console.log(linkEl.getAttribute('href'));;
 
-console.log(linkEl.attributes);
+// console.log(linkEl.attributes);
+
+const newTitle = document.createElement('h1');
+newTitle.textContent ='Это новый заголовок';
+// console.log(newTitle);
+navEl.before(newTitle);
+
+
+
+const newNavLink = document.createElement('li');
+newNavLink.textContent = 'Информация';
+newNavLink.classList.add('site-nav__item');
+// console.log(newNavLink);
+navEl.before(newNavLink);
+
+
+
+// Пример с видео_____________________________
+
+const colors = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+const colorPickerEl = document.querySelector('.js-simple');
+
+const makeColor = options => {
+    return options.map(option => {
+        const buttonEl = document.createElement('button');
+        buttonEl.type = 'button';
+        buttonEl.classList.add('color__button');
+        buttonEl.textContent = option.label;
+        buttonEl.style.backgroundColor = option.color;
+
+        return buttonEl;
+    });
+};
+
+const elements = makeColor(colors);
+
+colorPickerEl.append(...elements);
+
+
+
+
+
+
+
+
 
 
 
